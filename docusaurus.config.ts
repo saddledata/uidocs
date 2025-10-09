a7fc4ac0ee3bc9a635c6ace56907188de4a22d9b
Drew Buckman <drew.buckman@gmail.com>
2025-10-08 21:33:06 -0400

    fix baseurl

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -11,7 +11,7 @@ const config: Config = {
   url: 'https://saddledata.com',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/uidocs/',
+  baseUrl: '/',
 
   // GitHub pages deployment config.
   organizationName: 'saddledata',
