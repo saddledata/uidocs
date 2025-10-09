142cc7ebe86f0df84e919f6532043c934903c4d1
Drew Buckman <drew.buckman@gmail.com>
2025-10-08 21:39:35 -0400

    fix base redirect

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,5 +2,5 @@ import React from 'react';
 import { Redirect } from '@docusaurus/router';
 
 export default function Home() {
-  return <Redirect to="/uidocs/docs/getting-started/intro" />;
+  return <Redirect to="/docs/getting-started/intro" />;
 }
